6708849c18981bf7e99b3a3bfc6f9065b3be9d61
Shane Wilson <shanezilla@gmail.com>
2017-05-25 21:51:37 -0400

    ✨ add support for dotenv
    KNIT_WORKING_DIR
    KNIT_OUTPUT_DIR
    
    Closes #72

--- src/modules/@knit/needle/index.js
+++ src/modules/@knit/needle/index.js
@@ -1,4 +1,5 @@
 /* @flow */
+require("dotenv").config();
 
 const path = require("path");
 const readPkgUp = require("read-pkg-up");
@@ -9,8 +10,8 @@ const pkgNeedle = (pkg.knit || {}).needle || {};
 const pkgPaths = (pkgNeedle || {}).paths || {};
 const ROOT_DIR = process.env.PWD || "";
 const SRC_STUB = pkgPaths.src || "src";
-const MOD_STUB = pkgPaths.modulesStub || path.join(SRC_STUB, "modules");
-const DIST_STUB = pkgPaths.distStub || "dist";
+const MOD_STUB = process.env.KNIT_WORKING_DIR || path.join(SRC_STUB, "modules");
+const DIST_STUB = process.env.KNIT_OUTPUT_DIR || "dist";
 const TESTS_STUB = pkgPaths.testsStub || "__tests__";
 const DATA_STUB = pkgPaths.dataStub || "data";
 
