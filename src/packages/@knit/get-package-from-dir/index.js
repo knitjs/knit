a067283d2a2dc1b5096f481dd8758c344e7dc6e1
Shane Wilson <shanezilla@gmail.com>
2017-09-05 10:14:33 -0400

    🔥 remove more unused code

--- src/packages/@knit/get-package-from-dir/index.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-
-import path from "path";
-
-type TGetPackageFromDir = (d: string) => string;
-const getPackageFromDir: TGetPackageFromDir = dir => dir.replace(path.sep, "/");
-
-module.exports = getPackageFromDir;
