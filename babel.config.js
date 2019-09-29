23ce614e28b5fd55cc5ec8563fde7fcbb42ad5d7
Shane Wilson <shanezilla@gmail.com>
2019-09-29 15:46:22 -0400

    🛠run on node lts and stable

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
     "@babel/preset-flow",
-    ["@babel/preset-env", { targets: { node: 12 } }]
+    ["@babel/preset-env", { targets: { node: 10 } }]
   ],
   plugins: ["add-module-exports", ["array-includes", { useBuiltIns: true }]],
   babelrcRoots: [".", "src/packages/*"]
