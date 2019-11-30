5bd66a937e4a5b3728eb38529bbf3d30fe7f1596
Shane Wilson <shanezilla@gmail.com>
2019-11-29 22:06:54 -0500

    ✨auto release master, fetch latest pkg version from registry
    - updates release action to auto inc version
    - add a new magic var called KNIT_MODULE_VERSION that is replaced with the latest version from the registry
    - sets all versions to 0.0.0-monorepo since the version in code doesn't mater anymore

--- src/packages/@knit/knit/tasks/exec.js
+++ src/packages/@knit/knit/tasks/exec.js
@@ -4,6 +4,7 @@ import type { TPackages } from "@knit/find-packages";
 import type { TPackageNames } from "@knit/knit-core";
 
 const Listr = require("listr");
+const latestVersion = require("latest-version");
 
 const needle = require("@knit/needle");
 const pathJoin = require("@knit/path-join");
@@ -30,19 +31,29 @@ const tasks = [
             new Listr(
               ctx.modules.map(m => ({
                 title: m,
-                task: () => {
+                task: async () => {
                   // using $KNIT_MODULE_NAME as an env gets auto-expanded before we can get ahold of it
                   // zsh eats it so you need to escape \$KNIT_MODULE_NAME
                   // but before we get access it gets eaten again so
                   // need to escape like \\\$KNIT_MODULE_NAME - which is too many \ to bother with
-                  const args = ctx.args.map(x => {
-                    x = x.replace("KNIT_MODULE_NAME", m);
-                    x = x.replace("KNIT_MODULE_DIR", ctx.modulesMap[m].dir);
-                    x = x.replace("ROOT_DIR", needle.paths.rootDir);
-                    return x;
-                  });
+                  const args = Promise.all(
+                    ctx.args.map(async x => {
+                      x = x.replace("KNIT_MODULE_NAME", m);
+                      x = x.replace("KNIT_MODULE_DIR", ctx.modulesMap[m].dir);
+                      x = x.replace("ROOT_DIR", needle.paths.rootDir);
 
-                  return execa(ctx.cmd, args, {
+                      if (x.includes("KNIT_MODULE_VERSION")) {
+                        x = x.replace(
+                          "KNIT_MODULE_VERSION",
+                          await latestVersion(m)
+                        );
+                      }
+
+                      return x;
+                    })
+                  );
+
+                  return execa(ctx.cmd, await args, {
                     cwd: pathJoin(ctx.modulesMap[m].path)
                   });
                 }
