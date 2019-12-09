258a3cd9aec7e06a78d5ce3af573a992e963d903
Shane Wilson <shanezilla@gmail.com>
2019-12-09 16:36:48 -0500

    ✨create danger action for prerelease

---
+++ dangerfile.js
@@ -0,0 +1,4 @@
+import { prerelease } from "@knit/danger-action-prerelease";
+import { schedule } from "danger";
+
+schedule(prerelease);
---
+++ src/packages/@knit/danger-action-prerelease/index.js
@@ -0,0 +1,50 @@
+/* @flow */
+import { markdown } from "danger";
+
+import { findPublicPackages } from "@knit/find-packages";
+import {
+  findModifiedSince,
+  findModifiedPackages
+} from "@knit/find-modified-packages";
+import needle from "@knit/needle";
+import { normalizeBranch, currentBranch } from "@knit/git-branch-semver";
+
+const modulesMap = findPublicPackages(needle.paths.workspace);
+
+const modifiedSince = findModifiedSince(
+  needle.paths.workspace,
+  modulesMap,
+  "master.."
+);
+
+export const prerelease = async () => {
+  const modifiedPackages = await findModifiedPackages(
+    modulesMap,
+    modifiedSince
+  );
+
+  if (modifiedPackages.length) {
+    const branch = normalizeBranch(await currentBranch());
+
+    markdown(
+      `
+<details>
+<summary>We found that ${
+        modifiedPackages.length
+      } packages have been modified by this PR.</summary>
+<br/>
+
+\`\`\`    
+${modifiedPackages.join("\n")}
+\`\`\`
+</details>
+
+You can install the pre-release version of these packages by running:
+
+\`\`\`
+yarn add ${modifiedPackages.map(m => `${m}@${branch}`).join(" ")}
+\`\`\`
+`
+    );
+  }
+};
