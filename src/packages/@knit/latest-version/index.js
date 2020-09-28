e2742e011d0467dbf900551caec06858352f5920
Shane Wilson <shanezilla@gmail.com>
2020-09-28 14:57:14 -0400

    fallback when no latest

--- src/packages/@knit/latest-version/index.js
+++ src/packages/@knit/latest-version/index.js
@@ -4,9 +4,9 @@ import execa from "execa";
 type TLatestVersion = (pkg: string, fallback: string) => Promise<string>;
 export const latestVersion: TLatestVersion = (pkg, fallback) =>
   execa("npm", ["info", pkg, "version"])
-    .then(v => v.stdout)
+    .then(v => v.stdout.length === 0 ? fallback : v.stdout)
     .catch(err => {
-      if (err.stderr.includes("404")) {
+      if (err.stderr.includes("404") || err.stdout.length === 0) {
         return "0.0.0";
       }
       throw err;
