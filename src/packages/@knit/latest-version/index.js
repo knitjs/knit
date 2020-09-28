76f485e987280fd2951f2a7adeb15d1984ed3f75
Shane Wilson <shanezilla@gmail.com>
2020-09-28 15:02:34 -0400

    lint

--- src/packages/@knit/latest-version/index.js
+++ src/packages/@knit/latest-version/index.js
@@ -4,7 +4,7 @@ import execa from "execa";
 type TLatestVersion = (pkg: string, fallback: string) => Promise<string>;
 export const latestVersion: TLatestVersion = (pkg, fallback) =>
   execa("npm", ["info", pkg, "version"])
-    .then(v => v.stdout.length === 0 ? fallback : v.stdout)
+    .then(v => (v.stdout.length === 0 ? fallback : v.stdout))
     .catch(err => {
       if (err.stderr.includes("404") || err.stdout.length === 0) {
         return "0.0.0";
