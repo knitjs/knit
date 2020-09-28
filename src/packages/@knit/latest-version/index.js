0bdc32bdc45c143b8419df57c29d8e9e11b49494
Shane Wilson <shanezilla@gmail.com>
2020-09-28 13:27:47 -0400

    .

--- src/packages/@knit/latest-version/index.js
+++ src/packages/@knit/latest-version/index.js
@@ -1,13 +1,10 @@
 /* @flow */
 import execa from "execa";
 
-type TLatestVersion = (
-  pkg: string,
-  fallback: string
-) => Promise<string>;
-export const latestVersion: TLatestVersion = (pkg, fallback) => execa("npm", ["info", pkg, "version"])
+type TLatestVersion = (pkg: string, fallback: string) => Promise<string>;
+export const latestVersion: TLatestVersion = (pkg, fallback) =>
+  execa("npm", ["info", pkg, "version"])
     .catch(err => {
-      
       if (err.stderr.includes("404")) {
         return "0.0.0";
       }
