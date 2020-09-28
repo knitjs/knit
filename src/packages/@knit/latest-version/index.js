c720c4b89138ec680c8ac74ca4718f459754b84d
Shane Wilson <shanezilla@gmail.com>
2020-09-28 15:17:59 -0400

    🐛 only use version fallback on new packages (#141)
    * no fallback
    
    * throw err
    
    * win too long
    
    * .
    
    * use npm info
    
    * .
    
    * fallback on 404
    
    * .
    
    * type
    
    * .
    
    * .
    
    * fallback when no latest
    
    * lint

--- src/packages/@knit/latest-version/index.js
+++ src/packages/@knit/latest-version/index.js
@@ -1,12 +1,13 @@
 /* @flow */
-import lv from "latest-version";
+import execa from "execa";
 
-type TLatestVersion = (
-  pkg: string,
-  fallback: string,
-  options: any
-) => Promise<string>;
-export const latestVersion: TLatestVersion = (pkg, fallback, options) =>
-  lv(pkg, options)
-    .catch(() => fallback)
-    .then(v => v);
+type TLatestVersion = (pkg: string, fallback: string) => Promise<string>;
+export const latestVersion: TLatestVersion = (pkg, fallback) =>
+  execa("npm", ["info", pkg, "version"])
+    .then(v => (v.stdout.length === 0 ? fallback : v.stdout))
+    .catch(err => {
+      if (err.stderr.includes("404") || err.stdout.length === 0) {
+        return "0.0.0";
+      }
+      throw err;
+    });
